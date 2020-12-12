c4da65957c848864ff474e35a87d5ee93cf252db
Jhon-Romero-B <jhon.romero.betancourt@gmail.com>
2020-12-11 21:38:24 -0500

    Solo me falta redireccionar

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-function validacion() {}
+var correo = document.getElementById("email").value;
+var contraseña = document.getElementById("password").value;
 
-var email = document.getElementById("email").value;
-var password = document.getElementById("password").value;
-var objeto = {
-    correo: "jaromero374@misena.edu.co",
+const objeto = {
+    email: "jaromero374@misena.edu.co",
     password: "1234"
+
 }
 
-if (email.value === null || email.value === "") {
-    alert("Ingrese su correo");
-}
-if (password.value === null || password.value === "") {
-    alert("Ingrese su contraseña");
-}
-if ((email.value == "jaromero374@misena.edu.co") && (password.value == "1234")) {
-    location.href = "https://unicode-table.com/es/search/?q=cerradura";
-}
-return false;
+function validacion() {
+
+    if (email.value === null || email.value === "") {
+        alert("Ingrese su correo");
+    }
+    if (password.value === null || password.value === "") {
+        alert("Ingrese su contraseña");
+    }
+    if ((email.value == "jaromero374@misena.edu.co") && (password.value == "1234")) {
+        location.href = "https://unicode-table.com/es/search/?q=cerradura";
+    }
+    return false;
 }
