5d1ee439929a536353719736c89e516ecb6362fa
Jhon-Romero-B <jhon.romero.betancourt@gmail.com>
2020-12-11 21:32:56 -0500

    no message

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ function validacion() {}
 var email = document.getElementById("email").value;
 var password = document.getElementById("password").value;
 var objeto = {
-    email: "jaromero374@misena.edu.co",
+    correo: "jaromero374@misena.edu.co",
     password: "1234"
 }
 
 if (email.value === null || email.value === "") {
-    alert("Ingrese correo");
-
-    if (password.value === null || password.value === "") {
-        alert("Ingrese su contraseña");
-
-        if ((email.value == "jaromero374@misena.edu.co") && (password.value == "1234")) {
-            location.href = "https://unicode-table.com/es/search/?q=cerradura";
-        }
-        return false;
-    }
+    alert("Ingrese su correo");
+}
+if (password.value === null || password.value === "") {
+    alert("Ingrese su contraseña");
+}
+if ((email.value == "jaromero374@misena.edu.co") && (password.value == "1234")) {
+    location.href = "https://unicode-table.com/es/search/?q=cerradura";
+}
+return false;
+}
